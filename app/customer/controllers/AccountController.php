cab17fc586fa77a14bea0db8fe0857ff37666259
LiChunHua <lichunhua@medatc.com>
2018-05-23 11:35:32 +0800

    x

--- app/customer/controllers/AccountController.php
+++ app/customer/controllers/AccountController.php
@@ -66,6 +66,7 @@ class AccountController extends \yii\web\Controller
 
         if ($openId = $wxadv->get_open_id($code)) {
             $account = \common\customer\Account::findByOpenId($openId);
+            var_dump($account);exit;
 
             if (! $account) { // 自动注册
                 $account = $this->doWechatRegister($openId, $wxadv);
